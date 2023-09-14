5f0a83e69b8eeb18cdaa9822696439eb62a34530
Guapura89 <betocrack1228@gmail.com>
2023-09-14 12:19:04 -0600

    feat: add wallet provider :sparkles:

---
+++ src/context/WalletConnectioProvider.js
@@ -0,0 +1,23 @@
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { useMemo } from "react";
+
+const WalletConnectionProvider = ({ children }) => {
+  const endpoint = useMemo(() => "https://api.devnet.solana.com", []);
+
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets}>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default WalletConnectionProvider;
