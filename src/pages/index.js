0147d3e94f74e7b6f12ca4f3105c1150c5a9ecfd
Guapura89 <betocrack1228@gmail.com>
2023-09-14 14:13:58 -0600

    feat: add tailwind extend themes :sparkles:

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="w-full h-screen grid place-items-center">
-      <h1>Zoren app</h1>
+      <h1 className="">Zoren app</h1>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -24,9 +24,18 @@ module.exports = {
   plugins: [
     plugin(function ({ addBase, theme }) {
       addBase({
-        h1: { fontSize: theme("fontSize.3xl", "fontBold") },
-        h2: { fontSize: theme("fontSize.2xl", "fontBold") },
-        h3: { fontSize: theme("fontSize.lg", "fontBold") },
+        h1: {
+          fontSize: theme("fontSize.3xl"),
+          fontWeight: theme("fontWeight.bold"),
+        },
+        h2: {
+          fontSize: theme("fontSize.2xl"),
+          fontWeight: theme("fontWeight.bold"),
+        },
+        h3: {
+          fontSize: theme("fontSize.lg"),
+          fontWeight: theme("fontWeight.bold"),
+        },
       });
     }),
   ],
