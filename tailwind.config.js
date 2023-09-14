5b44e5dbaa54e034e24e948c1ca94da27014eea5
Guapura89 <betocrack1228@gmail.com>
2023-09-14 13:52:45 -0600

    feat: add tailwind extend themes :sparkles:

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,33 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "#535F8B",
+        secondary: "#8EA7E9",
+        light: "#FFF2F2",
+        alt: "#E5E0FF",
+        dark: "#2C3333",
+        success: "#96BB99",
+        danger: "#C77676",
+        alert: "#E5D283",
       },
     },
   },
-  plugins: [],
-}
+  plugins: [
+    plugin(function ({ addBase, theme }) {
+      addBase({
+        h1: { fontSize: theme("fontSize.3xl") },
+        h2: { fontSize: theme("fontSize.2xl") },
+        h3: { fontSize: theme("fontSize.lg") },
+      });
+    }),
+  ],
+};
