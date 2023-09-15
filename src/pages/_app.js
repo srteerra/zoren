f504d4c5ba6b52f01fff8b0673fc680ff7f5c87c
Angel Lopez <srterradev@gmail.com>
2023-09-15 14:12:09 -0600

    feat: next themes implemented

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { ThemeProvider } from "next-themes";
+import { appWithTranslation } from "next-i18next";
 
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
+
+export default appWithTranslation(App);
