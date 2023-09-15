c03fdaddde1784119ad1b4e99a883e0c2c103e0f
Guapura89 <betocrack1228@gmail.com>
2023-09-15 15:15:58 -0600

    fix: fix dark mode button icon :bug:

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -1,11 +1,18 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Fade } from "react-awesome-reveal";
 import { useTheme } from "next-themes";
 import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
+import { useEffect } from "react";
 
 const DarkMode = () => {
   const { theme, setTheme } = useTheme();
   const [click, setClick] = useState(false);
+
+  useEffect(() => {
+    theme === "dark" ? setClick(false) : setClick(true);
+  }, [])
+
 
   const handleClick = () => {
     setClick(!click);
