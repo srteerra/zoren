d18eab182fc9b2ebe2723d95d46113aaa45b9b57
Guapura89 <betocrack1228@gmail.com>
2023-09-15 13:49:38 -0600

    feat: join side navs into layout component :sparkles:

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,10 @@
-import '@/styles/globals.css'
-
+import "@/styles/globals.css";
+import Layout from "./layout";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,6 @@
-import { Profile } from "@/components/Profile";
-import { Collections } from "@/containers/Collections";
-
 export default function Home() {
   return (
     <main className="relative w-full h-screen">
-      <Profile />
       <h1 className="">Zoren app</h1>
     </main>
   );
