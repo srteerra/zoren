7081e6264e410c2b1ef005dc498d14bd715a2899
Guapura89 <betocrack1228@gmail.com>
2023-09-19 14:51:58 -0600

    feat: add Home page and login nav component :sparkles:

---
+++ src/components/LoginNav.jsx
@@ -0,0 +1,115 @@
+"use clients";
+import Image from "next/image";
+import { DarkMode, DarkModeMobile } from "./DarkMode";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import { Slide } from "react-awesome-reveal";
+import { useState } from "react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+// Images
+import logo from "../../public/logos/horizontal-color.png";
+import RegionChange from "./Region";
+
+const LoginNav = () => {
+  const [show, steShow] = useState(false);
+  const path = usePathname();
+
+  const active = "font-bold text-primary dark:text-secondary";
+
+  const slide = (
+    <div className="lg:hidden fixed z-20 top-0 right-0 w-full flex justify-end h-screen backdrop-brightness-75">
+      <Slide direction="right" className="w-11/12 sm:w-8/12 lg:w-1/2">
+        <div className="w-full h-full bg-white sm:rounded-l-2xl p-4 relative">
+          <button
+            onClick={() => steShow(!show)}
+            className="absolute top-6 sm:top-10 right-[46%] sm:right-6 text-dark dark:text-dark"
+          >
+            <XMarkIcon className="h-8 w-8" />
+          </button>
+          <div className="py-6 sm:py-0 flex flex-col items-center justify-around h-full">
+            <Link href={"/"} className="mx-auto">
+              <Image src={logo} width={100} height={100} alt="logo" />
+            </Link>
+            <div className="w-full text-dark flex justify-center items-center gap-4">
+              <DarkModeMobile />
+              <RegionChange />
+            </div>
+            <ul className="flex flex-col gap-y-10 items-center">
+              {[
+                ["Home", "/"],
+                ["How it works?", "/how"],
+                ["About us", "/about"],
+              ].map(([title, href]) => (
+                <li key={title}>
+                  <Link
+                    className={
+                      path === title
+                        ? active
+                        : "text-dark hover:text-secondary transition duration-150 ease-linear"
+                    }
+                    href={href}
+                  >
+                    {title}
+                  </Link>
+                </li>
+              ))}
+              <button className="bg-primary my-6 text-white hover:bg-secondary transition duration-150 ease-linear p-2 w-40 rounded-full">
+                Connect Wallet
+              </button>
+            </ul>
+          </div>
+        </div>
+      </Slide>
+    </div>
+  );
+
+  return (
+    <nav className="h-1/6 w-full">
+      {show ? slide : null}
+      <div className="w-4/5 h-full flex items-center justify-between mx-auto">
+        <div>
+          <Image
+            priority={true}
+            src={logo}
+            alt="logo"
+            width={90}
+            height={90}
+            className="w-auto"
+          />
+        </div>
+
+        <ul className="hidden lg:flex gap-x-10 items-center">
+          {[
+            ["Home", "/"],
+            ["How it works?", "/how"],
+            ["About us", "/about"],
+          ].map(([title, href]) => (
+            <li key={title}>
+              <Link
+                className={
+                  path === href
+                    ? active
+                    : "hover:text-secondary transition duration-150 ease-linear"
+                }
+                href={href}
+              >
+                {title}
+              </Link>
+            </li>
+          ))}
+          <DarkMode />
+          <button className="bg-primary text-white hover:bg-secondary transition duration-150 ease-linear p-2 w-40 rounded-full">
+            Connect Wallet
+          </button>
+        </ul>
+
+        <button onClick={() => steShow(!show)} className="block lg:hidden">
+          <Bars3Icon className="w-6 h-6" />
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export { LoginNav };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,141 @@
+"use clients";
+import { LoginNav } from "@/components/LoginNav";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { ViewfinderCircleIcon } from "@heroicons/react/24/solid";
+
+// Images
+import ticket from "../../public/images/ticket.png";
+import friends from "../../public/images/friends.png";
+import Image from "next/image";
+import { Zoom } from "react-awesome-reveal";
+
 const Home = () => {
-  return <h1>Home</h1>;
+  const [show, setShow] = useState("friends");
+
+  const words = ["family", "mates", "friends"];
+
+  useEffect(() => {
+    const changeWords = () => {
+      let i = 0;
+      setInterval(() => {
+        setShow(words[i]);
+        if (i === 2) {
+          i = 0;
+        } else i++;
+      }, 5000);
+    };
+
+    changeWords();
+  }, []);
+
+  return (
+    <div className="w-full h-screen">
+      <LoginNav />
+
+      {/* Fisrt section */}
+      <div className="w-full lg:w-4/5 mx-auto flex h-5/6 items-center">
+        {/* left side */}
+        <div className="w-full text-center md:text-start md:w-1/2 p-8 lg:p-0 overflow-hidden">
+          <div className="">
+            <p className="text-slate-500 my-4">Want to separate the bill?</p>
+            <div>
+              <p className="text-4xl lg:text-5xl font-light">
+                Generate QR codes to <br /> split bill payments <br /> between
+                your
+                <span className="text-primary font-bold"> {show}</span>.
+              </p>
+            </div>
+
+            <p className="my-6">
+              Using{" "}
+              <Link href={"/"}>
+                <strong>Solana</strong>
+              </Link>{" "}
+              and
+              <Link href={"/"}>
+                {" "}
+                <strong>Phantom</strong>
+              </Link>
+            </p>
+          </div>
+          <div className="w-full flex justify-center md:justify-start items-center gap-2 my-6">
+            <button className="bg-secondary focus:bg-secondary hover:bg-primary transition suration-150 ease-linear p-2 w-36 rounded-full text-white font-bold">
+              Ty it now
+            </button>
+            <Link href={"/"}>Learn more</Link>
+          </div>
+
+          <div className="my-14 text-sm">
+            <p>
+              Please check our{" "}
+              <Link href={"/"} className="font-bold">
+                Privacy policy
+              </Link>{" "}
+              and Terms and consitions.
+            </p>
+            <p>This is a project for Hyperdrive Hackaton.</p>
+          </div>
+        </div>
+
+        {/* right side */}
+        <div className="w-1/2 h-full hidden md:flex pb-10">
+          <div className="w-full grid grid-cols-2">
+            <div className="relative">
+              <div className="grid grid-rows-3 p-4 h-full">
+                <div className="row-span-2 grid grid-rows-4">
+                  <div className="h-full grid place-items-center relative row-span-2 row-start-2 max-h-[250px] lg:bg-secondary rounded-2xl ">
+                    <Image
+                      src={ticket}
+                      alt="tickets"
+                      style={{ width: "60%" }}
+                      priority
+                      quality={100}
+                      className="2xl:absolute bottom-0 left-[20%]"
+                    />
+                  </div>
+                </div>
+                <span className="absolute bottom-40 left-32">
+                  <Zoom triggerOnce>
+                    <ViewfinderCircleIcon className="h-16 w-16" />
+                  </Zoom>
+                </span>
+              </div>
+            </div>
+            <div className="relative">
+              <span className="absolute top-40 left-14">
+                <Zoom triggerOnce>
+                  <ViewfinderCircleIcon className="h-12 w-12" />
+                </Zoom>
+              </span>
+              <div className="grid grid-rows-3 h-full">
+                <div className="row-span-2 row-start-2 grid grid-rows-3 ">
+                  <div className="h-full grid place-items-center relative row-span-2 max-h-[300px] row-start-2 lg:bg-secondary rounded-2xl ">
+                    <Image
+                      src={friends}
+                      alt="tickets"
+                      style={{ width: "60%" }}
+                      priority
+                      quality={100}
+                      className="2xl:absolute bottom-4 left-[20%]"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-primary flex flex-col justify-between overflow-hidden h-screen w-full text-white">
+        <div className="h-2/6 flex text-center flex-col w-full sm:w-1/2 px-10 py-20 lg:w-1/3 mx-auto">
+          <h1 className="lg:text-5xl">For whatever the occasion</h1>
+          <p>You can use Zoren form your phone or PC</p>
+        </div>
+        <div className="h-3/6 flex items-end justify-center bg-[url('../../public/images/banner.png')] bg-no-repeat bg-cover lg:bg-contain bg-center"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
