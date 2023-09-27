f48879648028c3a255d41c6617c24e83036ac442
Angel Lopez <srterradev@gmail.com>
2023-09-26 23:47:45 -0600

    fix: state removal from index ag

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use clients";
 import { LoginNav } from "@/components/LoginNav";
 import Link from "next/link";
@@ -30,7 +31,6 @@ const Home = () => {
   const { connected, publicKey } = useZoren();
 
   useEffect(() => {
-    setChangeWallet(true);
     if (connected) {
       router.push("/dashboard");
     }
